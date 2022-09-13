ff34575aa0c53c996dee1f74e9333efcd881bcf9
Kiiroi <sammydevjs@gmail.com>
2022-09-13 07:27:44 -0300

    inicio do projeto

---
+++ offtank.js
@@ -0,0 +1,343 @@
+
+
+
+
+const ListaDeOffTanks = [
+// PLANTA
+{
+  icone: "./img/grass.png",
+  icone2: "",
+  nome: "Gogoat",
+  imagem: "./img/Gogoat.gif",
+  elemento: "planta",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone: "./img/grass.png",
+  icone2: "",
+  nome: "Sceptile",
+  imagem: "./img/Sceptile.gif",
+  elemento: "planta",
+  funcao: "offtank",
+  level: "80",
+  tier: "3"
+},
+
+// LUTADOR
+{
+  icone: "./img/lutador.png",
+  icone2: "",
+  nome: "Elite Hitmontop",
+  imagem: "./img/Elitehitmontop.gif",
+  elemento: "lutador",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone: "./img/lutador.png",
+  icone2: "",
+  nome: "Mega Lopunny",
+  imagem: "./img/MegaLopunny.gif",
+  elemento: "lutador",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// RAIBOLT
+{
+  icone: "./img/eletrico.png",
+  icone2: "",
+  nome: "Electabuzz",
+  imagem: "./img/Electabuzz.gif",
+  elemento: "elétrico",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone: "./img/eletrico.png",
+  icone2: "",
+  nome: "Light Ball Pikachu",
+  imagem: "./img/LightBallPikachu.gif",
+  elemento: "elétrico",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// FOGO 
+{
+  icone: "./img/fogo.png",
+  icone2: "",
+  nome: "Arcanine",
+  imagem: "./img/Arcanine.gif",
+  elemento: "fogo",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone: "./img/fogo.png",
+  icone2: "",
+  nome: "Magmar",
+  imagem: "./img/Magmar.gif",
+  elemento: "fogo",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// ÁGUA
+{
+  icone: "./img/agua.png",
+  icone2: "",
+  nome: "Shiny Vaporeon",
+  imagem: "./img/ShinyVaporeon.gif",
+  elemento: "água",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone: "./img/agua.png",
+  icone2: "",
+  nome: "Shiny Giant Magikarp",
+  imagem: "./img/ShinyMagikarp.gif",
+  elemento: "água",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// PEDRA
+{
+  icone: "./img/Pedra.png",
+  icone2: "",
+  nome: "Shiny Golem",
+  imagem: "./img/ShiGolem.gif",
+  elemento: "pedra",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone: "./img/Pedra.png",
+  icone2: "./img/agua.png",
+  nome: "Omastar",
+  imagem: "./img/Omastar.gif",
+  elemento: ["água","pedra"],
+  funcao: "offtank",
+  level: "80",
+  tier: "3"
+},
+
+// VOADOR
+{
+  icone: "./img/voador.png",
+  icone2: "",
+  nome: "Shiny Fearow",
+  imagem: "./img/ShiFearow.gif",
+  elemento: ["normal","voador"],
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{ icone:"./img/voador.png",
+  icone2:"./img/inseto.png",
+  nome: "Shiny Beautifly",
+  imagem: "./img/ShinyBeautifly.gif",
+  elemento: ["voador", "inseto"],
+  funcao: "offtank",
+  level: "80",
+  tier: "3"
+},
+
+// VENENO 
+{ 
+  icone:"./img/veneno.png",
+  icone2: "",
+  nome: "Shiny Arbok",
+  imagem: "./img/ShinyArbok.gif",
+  elemento: "veneno",
+  funcao: "offtank",
+  level: "80",
+  tier: "3"
+},
+{
+  icone:"./img/veneno.png",
+  icone2: "",
+  nome: "Shiny Qwilfish",
+  imagem: "./img/ShinyQwilfish.gif",
+  elemento: "veneno",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// FADA
+{
+  icone:"./img/fada.png",
+  icone2: "",
+  nome: "Florges",
+  imagem: "./img/Florges.gif",
+  elemento: "fada",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone:"./img/fada.png",
+  icone2: "",
+  nome: "Sylveon",
+  imagem: "./img/Sylveon.gif",
+  elemento: "fada",
+  funcao: "offtank",
+  level: "80",
+  tier: "3"
+},
+
+// PSIQUICO
+{
+  icone:"./img/psiquico.png",
+  icone2: "",
+  nome: "Wobbuffet",
+  imagem: "./img/Wobbuffet.gif",
+  elemento: "psíquico",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// DARK
+{
+  icone:"./img/dark.png",
+  icone2: "",
+  nome: "Mightyena",
+  imagem: "./img/Mightyena.gif",
+  elemento: "dark",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// TERRA
+{
+  icone:"./img/terra.png",
+  icone2: "./img/metal.png",
+  nome: "Steelix",
+  imagem: "./img/Steelix.gif",
+  elemento: ["terra", "metal"],
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// METAL
+{
+  icone:"./img/metal.png",
+  icone2: "",
+  nome: "Shiny Forretress",
+  imagem: "./img/ShinyForretress.gif",
+  elemento: "metal",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone:"./img/metal.png",
+  icone2: "./img/psiquico.png",
+  nome: "Metang",
+  imagem: "./img/Metang.gif",
+  elemento: ["metal", "psíquico"],
+  funcao: "offtank",
+  level: "80",
+  tier: "3"
+},
+// GELO
+{
+  icone:"./img/gelo.png",
+  icone2: "",
+  nome: "Beartic",
+  imagem: "./img/Beartic.gif",
+  elemento: "gelo",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// INSETO
+{
+  icone:"./img/inseto.png",
+  icone2: "./img/voador.png",
+  nome: "Vespiquen",
+  imagem: "./img/vespiquen.gif",
+  elemento: ["inseto", "voador"],
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone:"./img/inseto.png",
+  icone2: "",
+  nome: "Accelgor",
+  imagem: "./img/Accelgor.gif",
+  elemento: "inseto",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+
+// FANTASMA
+{
+  icone: "./img/fantasma.png",
+  icone2: "",
+  nome: "Cofagrigus",
+  imagem: "./img/Cofagrigus.gif",
+  elemento: "fantasma",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+},
+{
+  icone: "./img/fantasma.png",
+  icone2: "",
+  nome: "Mismagius",
+  imagem: "./img/mismagius.gif",
+  elemento: "fantasma",
+  funcao: "offtank",
+  level: "100",
+  tier: "2"
+}
+];
+
+// ZONA DE TESTE
+const imagemPoke = document.querySelectorAll('.imagemPoke');
+// imagemPoke.src = mismagius.imagem;
+const nomePoke = document.querySelectorAll('.tituloPoke');
+// nomePoke.textContent = mismagius.nome;
+const iconePoke = document.querySelectorAll('.iconeElemento');
+const iconePoke2 = document.querySelectorAll('.iconeElemento2');
+const elementoPoke = document.querySelectorAll('.elemento');
+const funcaoPoke = document.querySelectorAll('.funcao');
+const levelPoke = document.querySelectorAll('.level');
+const tierPoke = document.querySelectorAll('.tier');
+
+for (let i = 0; i < ListaDeOffTanks.length; i++) {
+  iconePoke[i].src = ListaDeOffTanks[i].icone;
+  imagemPoke[i].src = ListaDeOffTanks[i].imagem;
+  nomePoke[i].textContent = ListaDeOffTanks[i].nome;
+  elementoPoke[i].textContent = ListaDeOffTanks[i].elemento;
+  funcaoPoke[i].textContent = ListaDeOffTanks[i].funcao;
+  levelPoke[i].textContent = ListaDeOffTanks[i].level;
+  tierPoke[i].textContent = ListaDeOffTanks[i].tier;
+  if(ListaDeOffTanks[i].icone2 === ""){
+    iconePoke2[i].classList.add("corta")
+  } else {
+    iconePoke2[i].src = ListaDeOffTanks[i].icone2;
+  }
+  
+}
